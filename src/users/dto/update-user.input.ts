488a7839fe314fe71d6d84a9012052bc82dc026b
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-02-06 11:08:11 +0700

    feat + fix: fix wrong typing add validation

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,11 @@
 import { CreateUserInput } from './create-user.input';
 import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
   @Field(() => ID)
+  @IsString()
+  @IsNotEmpty()
   id: string;
 }
